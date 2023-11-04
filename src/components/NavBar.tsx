25999cc2b55d202aff841d872e913144bca9327c
saleempirwani <msr.pirwani@gmail.com>
2023-11-04 20:13:18 +0500

    navbar completed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,44 @@
+import { navLinks } from "@/constants";
+import Image from "next/image";
 import React from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="w-full absolute z-10 padding-x py-8">
+      <nav className="flex items-center justify-between max-container">
+        <a href="">
+          <Image
+            src="/assets/images/header-logo.svg"
+            alt="headerLogo"
+            width={129}
+            height={29}
+            className="object-contain"
+          />
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item, index) => (
+            <li key={index} className="">
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="hidden max-lg:block">
+          <Image
+            src="/assets/icons/hamburger.svg"
+            alt="menuIcon"
+            width={25}
+            height={25}
+          />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
