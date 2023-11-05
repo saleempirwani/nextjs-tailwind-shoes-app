7f41409b8a3b681e272104a9e49d161865707e3f
saleempirwani <msr.pirwani@gmail.com>
2023-11-05 18:43:58 +0500

    super quality components done

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,13 +5,15 @@ const Button = ({ label = "", iconUrl = "" }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red border-coral-red rounded-full text-white">
       {label}
-      <Image
-        src={iconUrl}
-        alt="iconUrl"
-        width={5}
-        height={5}
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconUrl && (
+        <Image
+          src={iconUrl}
+          alt="iconUrl"
+          width={5}
+          height={5}
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
--- src/sections/SuperQuality.tsx
+++ src/sections/SuperQuality.tsx
@@ -1,7 +1,45 @@
+import Button from "@/components/Button";
+import Image from "next/image";
 import React from "react";
 
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex-flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you <span className="text-coral-red">Super</span>{" "}
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Our carefully crafted footwear guarantees superior comfort and style.
+          Created with meticulous attention to detail, our shoes are intended to
+          enhance your experience, offering unparalleled quality, innovation,
+          and a hint of sophistication.
+        </p>
+
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+
+      <div className="flex flex-1 justify-center items-center">
+        <Image
+          src="/assets/images/shoe8.svg"
+          alt="iconUrl"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
