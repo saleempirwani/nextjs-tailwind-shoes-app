fb0b10f0feeb90fef6ff7fa2fb1e2b6474f40609
saleempirwani <msr.pirwani@gmail.com>
2023-11-09 13:48:39 +0500

    footer section completed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,19 +1,3 @@
-// import { shieldTick, support, truckFast } from "../assets/icons";
-// import {
-//   bigShoe1,
-//   bigShoe2,
-//   bigShoe3,
-//   customer1,
-//   customer2,
-//   shoe4,
-//   shoe5,
-//   shoe6,
-//   shoe7,
-//   thumbnailShoe1,
-//   thumbnailShoe2,
-//   thumbnailShoe3,
-// } from "../assets/images";
-
 export const navLinks = [
   { href: "#home", label: "Home" },
   { href: "#about-us", label: "About Us" },
@@ -101,5 +85,42 @@ export const reviews = [
     rating: "4.5",
     feedback:
       "The product not only met but exceeded my expectations. I'll definitely be a returning customer!",
+  },
+];
+
+export const socialMedia = [
+  { src: "/assets/icons/facebook.svg", alt: "facebook logo" },
+  { src: "/assets/icons/twitter.svg", alt: "twitter logo" },
+  { src: "/assets/icons/instagram.svg", alt: "instagram logo" },
+];
+
+export const footerLinks = [
+  {
+    title: "Products",
+    links: [
+      { name: "Air Force 1", link: "/" },
+      { name: "Air Max 1", link: "/" },
+      { name: "Air Jordan 1", link: "/" },
+      { name: "Air Force 2", link: "/" },
+      { name: "Nike Waffle Racer", link: "/" },
+      { name: "Nike Cortez", link: "/" },
+    ],
+  },
+  {
+    title: "Help",
+    links: [
+      { name: "About us", link: "/" },
+      { name: "FAQs", link: "/" },
+      { name: "How it works", link: "/" },
+      { name: "Privacy policy", link: "/" },
+      { name: "Payment policy", link: "/" },
+    ],
+  },
+  {
+    title: "Get in touch",
+    links: [
+      { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+      { name: "+92554862354", link: "tel:+92554862354" },
+    ],
   },
 ];
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,69 @@
+import { footerLinks, socialMedia } from "@/constants";
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-container">
+      <div className="flex flex-wrap justify-between gap-20 max-lg:flex-col">
+        <div className="flex flex-col">
+          <a href="/">
+            <Image
+              src="/assets/images/footer-logo.svg"
+              alt="footer"
+              width={150}
+              height={46}
+            />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm ">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((item, index) => (
+              <div
+                key={index}
+                className="bg-white flex items-center justify-center rounded-full w-12 h-12"
+              >
+                <Image src={item.src} alt={item.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+
+          <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap mt-8">
+            {footerLinks.map((item, index) => (
+              <div key={index} className="">
+                <h4 className="text-white font-montserrat font-medium text-2xl leading-normal mb-6">
+                  {item.title}
+                </h4>
+                <ul>
+                  {item.links.map((link, linkIndex) => (
+                    <li
+                      key={linkIndex}
+                      className="mt-3 text-base leading-normal font-montserrat "
+                    >
+                      <a href="/" className="text-white hover:text-white-400">
+                        {link.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between text-white-400 mt-24  max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 items-center gap-2 font-montserrat cursor-pointer ">
+          &copy;
+          <p>Copyright. All rights reserved</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms and Conditions</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
