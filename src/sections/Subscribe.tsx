110f39120533c74fe16a1c0210478e66d667c9df
saleempirwani <msr.pirwani@gmail.com>
2023-11-09 13:12:15 +0500

    subscribe section added

--- src/sections/Subscribe.tsx
+++ src/sections/Subscribe.tsx
@@ -1,7 +1,29 @@
+import Button from "@/components/Button";
 import React from "react";
 
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      id="contact-us"
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign Up from <span className="text-coral-red">Updates</span> &
+        Newsletter
+      </h3>
+
+      <div className="lg:max-w-[40%] w-full flex  justify-between lg:justify-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-text rounded-full">
+        <input
+          type="email"
+          className="outline-none"
+          placeholder="subscribe@nike.com"
+        />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" className="w-full" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
